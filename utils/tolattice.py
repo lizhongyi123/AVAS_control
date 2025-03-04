f51cd0a91fe85ce11bb4d8e546a647da94f6f44e
lizhongyi <1765959713@qq.com>
2025-03-04 10:29:37 +0800

    Update tolattice.py

--- utils/tolattice.py
+++ utils/tolattice.py
@@ -7,7 +7,7 @@ def write_mulp_to_lattice_only_sim(lattice_mulp_path, lattice_path):
     :return: None
     该函数的作用是将lattice_mulp中的内容写的入到lattice, 处理的情况为只进行多粒子模拟，不包含误差，矫正等
     """
-    lattice_mulp = read_lattice_mulp(lattice_mulp_path, out='list')
+    lattice_mulp = read_lattice_mulp(lattice_mulp_path)
     lattice_mulp = [i for i in lattice_mulp if i[0] in global_varible.mulp_basic_command]
     with open(lattice_path, 'w', encoding='utf-8') as f:
         for i in lattice_mulp:

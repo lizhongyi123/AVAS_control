972e863bc5a95a2548349158a5a527a252ec3309
lizhongyi_imp <lizhongyi_imp@noreply.gitcode.com>
2025-05-14 22:13:46 +0800

    5143

--- hpc/get_errordata.py
+++ hpc/get_errordata.py
@@ -1,24 +1,35 @@
 import os
 from utils.tool import format_output
 def get_errordata(**item):
-    error_exist = False
+
     project_path = item['projectPath']
     err_path = os.path.join(project_path, "OutputFile", "tmp", "errordata.log")
-    with open(err_path, 'r', encoding="UTF-8") as file:
-        text = file.read()
-        print(9, text)
-        if text.strip():  # 去除空格和换行后仍有内容
-            error_exist = True
+
+    #判断文件是否存在
+    if os.path.exists(err_path):
+        error_exist = False
+        with open(err_path, 'r', encoding="UTF-8") as file:
+            text = file.read()
+            print(9, text)
+            if text.strip():  # 去除空格和换行后仍有内容
+                error_exist = True
+            else:
+                error_exist = False
+
+        if error_exist:
+            kwargs = {"errorInfo": text, "ifFinish": 2 }
         else:
-            error_exist = False
+            kwargs = {"errorInfo": "", "ifFinish": 2 }
 
-    if error_exist:
-        kwargs = {"errorInfo": text}
+        output = format_output(**kwargs)
+
     else:
-        kwargs = {"errorInfo": ""}
+    #如果文件不存在,证明还没有模拟完
+        kwargs = {"errorInfo": "", "ifFinish": 1}
+        output = format_output(**kwargs)
 
-    output = format_output(**kwargs)
     return output
+
 
 
 if __name__ == '__main__':

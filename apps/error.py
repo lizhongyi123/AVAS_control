2e2fb07673bd157002a1fda06e26ed4b83252876
lizhongyi_imp <lizhongyi_imp@noreply.gitcode.com>
2025-05-16 15:44:45 +0800

    5163

--- apps/error.py
+++ apps/error.py
@@ -572,7 +572,7 @@ class Error():
         item = {
             "project_path": p_path,
             "output_file": os.path.join(p_path, out_putfile_),
-            "field_file": self.field_path,
+            "field_path": self.field_path,
             "errorlog_path": os.path.join(p_path, r'OutputFile/error_middle/output_0/ErrorLog.txt'),
         }
         multiparticle_obj = MultiParticle(item)
